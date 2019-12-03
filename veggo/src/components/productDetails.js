eb2a2ca435ed681b83e86d5cda5d7cab7a9e6229
lucasahlgren <35539195+lucasahlgren@users.noreply.github.com>
2019-12-03 22:39:17 +0100

    product page

--- veggo/src/components/productDetails.js
+++ veggo/src/components/productDetails.js
@@ -45,49 +45,47 @@ class ProductDetails extends Component {
 								</div>
 							</div>
 							<div className="row pt-4">
-								
-								<div className="col-md-5">
+								<div className="col-md-6">
 									<h3>{name_swe}</h3>
-									<div className="col-12">
-										<img
-											src={require("../images/" + this.props.detail.image)}
-											alt={this.props.detail.name}
-											className="img-fluid"
-										/>
-									</div>
-								
-									</div>
-									<div className="col-md-7 pt-md-0 pt-4">
-								<div className="row">
-										<div className="col-12 p-0">
-											<RadarChart data={this.props.radarChart} />
+									<div className="row p-md-2">
+										<div className="col-md-8 mx-auto">
+											<img
+												src={require("../images/" + this.props.detail.image)}
+												alt={this.props.detail.name}
+												className="img-fluid"
+											/>
 										</div>
-										</div>
-										<div className="row">
+									</div>
+									<div className="row p-2">
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.water_footprint.ranking)}} className="col-lg-4 col-6">{`Vattenfotavtryck: ${this.props.detail.water_footprint.value} ${this.props.detail.water_footprint.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.emissions.ranking)}} className="col-lg-4 col-6">{`Växthusgasutsläpp: ${this.props.detail.emissions.value} ${this.props.detail.emissions.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.land_use.ranking)}} className="col-lg-4 col-6">{`Markanvändning: ${this.props.detail.land_use.value} ${this.props.detail.land_use.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.energy.ranking)}} className="col-lg-4 col-6">{`Energiförbrukning: ${this.props.detail.energy.value} ${this.props.detail.energy.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.freshwater_toxicity.ranking)}} className="col-lg-4 col-6">{`Sötvattenförgiftning: ${this.props.detail.freshwater_toxicity.value} ${this.props.detail.freshwater_toxicity.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.fossil_depletion.ranking)}} className="col-lg-4 col-6">{`Fossil utarmning: ${this.props.detail.fossil_depletion.value} ${this.props.detail.fossil_depletion.unit}`}</p>
-											<p style={{backgroundColor: this.colorRanking(this.props.detail.freshwater_eutrophication.ranking)}} className="col-lg-4 col-6">{`Sörvattenförsurning: ${this.props.detail.freshwater_eutrophication.value} ${this.props.detail.freshwater_eutrophication.unit}`}</p>
+											<p style={{backgroundColor: this.colorRanking(this.props.detail.freshwater_eutrophication.ranking)}} className="col-lg-4 col-6">{`Sötvattenförsurning: ${this.props.detail.freshwater_eutrophication.value} ${this.props.detail.freshwater_eutrophication.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.marine_eutrophication.ranking)}} className="col-lg-4 col-6">{`Marinförsurning: ${this.props.detail.marine_eutrophication.value} ${this.props.detail.marine_eutrophication.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.terrestrial_acidification.ranking)}} className="col-lg-4 col-6">{`Markförsurning: ${this.props.detail.terrestrial_acidification.value} ${this.props.detail.terrestrial_acidification.unit}`}</p>
 											<p style={{backgroundColor: this.colorRanking(this.props.detail.terrestrial_toxicity.ranking)}} className="col-lg-4 col-6">{`Markförgiftning: ${this.props.detail.terrestrial_toxicity.value} ${this.props.detail.terrestrial_toxicity.unit}`}</p>
-										</div>	
-										
+										</div>									
+									</div>
+									<div className="col-md-6 pt-md-0 pt-4">
+										<div className="row">
+												<div className="col-12 p-0">
+													<RadarChart data={this.props.radarChart} />
+												</div>
+										</div>
+									</div>
 								</div>
-							
 							</div>
 						</div>
-					</div>
-				);
-			} else {
-				return <Redirect to="/" />;
-			}
-		} else return null;
+					);
+				} else {
+					return <Redirect to="/" />;
+				}
+			} else return null;
+		}
 	}
-}
 
 const mapStateToProps = state => {
 	console.log(state);
