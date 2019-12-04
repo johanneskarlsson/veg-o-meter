88f44411efcf902c0738955b02542041722980c0
lucasahlgren <35539195+lucasahlgren@users.noreply.github.com>
2019-12-04 15:23:25 +0100

    template for bg video

--- veggo/src/components/home.js
+++ veggo/src/components/home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+//import bg_video from "../videos/bg_video.mp4"
 
 export default class Home extends Component {
 	render() {
 		return (
             <div className="home">
             <div className="vh-80 d-flex justify-content-center align-items-center">
+				{/*<video className="bg-video" playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop"><source src={bg_video} type="video/mp4" /></video>*/}
 			<div className="container pb-5">
 				<div className="row">
 					<div className="col-md-8 p-3 mx-auto">
